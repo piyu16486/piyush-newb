b239a397dfd58cc85454094bee879294fe131bf8
nativeCephei <native1@cepheiinfotech.com>
2025-03-08 19:08:43 +0530

    add drawer library

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-/**
- * @format
- */
-
+// Only import react-native-gesture-handler on native platforms
+import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
